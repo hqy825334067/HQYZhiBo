ae0af2e290f2e9a3c9f04b6f86ba15823a409a96
HQY <825334067@qq.com>
2017-05-16 20:01:22 +0800

    初始化项目

--- DouYuZB/DouYuZB/ViewController.swift
+++ DouYuZB/DouYuZB/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var a : Int = 0
+        
     }
 
     override func didReceiveMemoryWarning() {
